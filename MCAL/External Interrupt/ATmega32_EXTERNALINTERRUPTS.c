52dd8e5c19bb3ade2b4093311f94cac763611b45
Ahmed Aref <ahmed.aref.omaira@gmail.com>
2023-07-31 06:26:58 +0300

    Added the external interrupts driver

--- MCAL/External Interrupt/ATmega32_EXTERNALINTERRUPTS.c
+++ MCAL/External Interrupt/ATmega32_EXTERNALINTERRUPTS.c
@@ -2,5 +2,137 @@
  * ATmega32_EXTERNALINTERRUPTS.c
  *
  * Created: 7/30/2023 3:56:17 AM
- *  Author: ahmed
+ *  Author: Ahmed Aref Omaira
  */ 
+/******************************************
+ *                                        *
+ *                INCLUDES                *
+ * 										  *
+ ******************************************/
+#include "ATmega32_EXTERNALINTERRUPTS.h"
+
+
+/*********************************************
+ *                                           *
+ *         IRQ HANDLERS DEFINITION           *
+ * 							    			 *
+ *********************************************/
+ISR(EXTI0_IRQHandler) {
+	if (GPtr_IRQCallBack[0] != NULL) {
+		//Call Back C function() which will be called once IRQ happen
+		GPtr_IRQCallBack[0]();
+		//This bit is cleared by writing a ‘1’ into the bit
+		SET_BIT(GIFR, GIFR_INTF0_BIT);
+	}
+}
+
+ISR(EXTI1_IRQHandler) {
+	if (GPtr_IRQCallBack[1] != NULL) {
+		//Call Back C function() which will be called once IRQ happen
+		GPtr_IRQCallBack[1]();
+		//This bit is cleared by writing a ‘1’ into the bit
+		SET_BIT(GIFR, GIFR_INTF1_BIT);
+	}
+}
+
+ISR(EXTI2_IRQHandler) {
+	if (GPtr_IRQCallBack[2] != NULL) {
+		//Call Back C function() which will be called once IRQ happen
+		GPtr_IRQCallBack[2]();
+		//This bit is cleared by writing a ‘1’ into the bit
+		SET_BIT(GICR, GICR_INT2_BIT);
+	}
+}
+
+/******************************************
+ *                                        *
+ *         FUNCTIONS DEFINITION           *
+ * 										  *
+ * ****************************************/
+/*******************************************
+ *      EXTERNALLY USED FUNCITONS          *
+ * *****************************************/
+void MCAL_EXTI_Enable( EXTI_InterruptConfig_t* interruptConfiguration){
+			// 1.Enable the Global Interrupt
+			Enable_GlobalInterrupt();
+			// 2.Choose which external interrupt signal 
+			switch (interruptConfiguration->EXTI_LineNumber) {
+				case EXTI_LINE0:
+				// 3.Enable the choosen external interrupt in the General Interupt Control Register
+				SET_BIT(GICR, GICR_INT0_BIT);
+				// 4.Pass the Call Back Pointer to Function to the global array to be exectued by the ISR
+				GPtr_IRQCallBack[0] = interruptConfiguration->P_IRQ_CallBack;
+				// 5.Choose the Interrupt Sense
+				switch (interruptConfiguration->EXTI_TriggerCase) {
+					case EXTI_LOW_LEVEL:
+					CLR_BIT(MCUCR, MCUCR_ISC00_BIT);
+					CLR_BIT(MCUCR, MCUCR_ISC01_BIT);
+					break;
+					case EXTI_ON_CHANGE:
+					SET_BIT(MCUCR, MCUCR_ISC00_BIT);
+					CLR_BIT(MCUCR, MCUCR_ISC01_BIT);
+					break;
+					case EXTI_FALLING_EDGE:
+					CLR_BIT(MCUCR, MCUCR_ISC00_BIT);
+					SET_BIT(MCUCR, MCUCR_ISC01_BIT);
+					break;
+					case EXTI_RISING_EDGE:
+					CLR_BIT(MCUCR, MCUCR_ISC00_BIT);
+					SET_BIT(MCUCR, MCUCR_ISC01_BIT);
+					break;
+				}
+				break;
+
+				case EXTI_LINE1:
+				GPtr_IRQCallBack[1] = interruptConfiguration->P_IRQ_CallBack;
+				SET_BIT(GICR, GICR_INT1_BIT);
+				switch (interruptConfiguration->EXTI_TriggerCase) {
+					case EXTI_LOW_LEVEL:
+					CLR_BIT(MCUCR, MCUCR_ISC10_BIT);
+					CLR_BIT(MCUCR, MCUCR_ISC11_BIT);
+					break;
+					case EXTI_ON_CHANGE:
+					SET_BIT(MCUCR, MCUCR_ISC10_BIT);
+					CLR_BIT(MCUCR, MCUCR_ISC11_BIT);
+					break;
+					case EXTI_FALLING_EDGE:
+					CLR_BIT(MCUCR, MCUCR_ISC10_BIT);
+					SET_BIT(MCUCR, MCUCR_ISC11_BIT);
+					break;
+					case EXTI_RISING_EDGE:
+					CLR_BIT(MCUCR, MCUCR_ISC10_BIT);
+					SET_BIT(MCUCR, MCUCR_ISC11_BIT);
+					break;
+				}
+				break;
+
+				case EXTI_LINE2:
+				GPtr_IRQCallBack[2] = interruptConfiguration->P_IRQ_CallBack;
+				SET_BIT(GICR, GICR_INT2_BIT);
+				switch (interruptConfiguration->EXTI_TriggerCase) {
+					case EXTI_FALLING_EDGE:
+					CLR_BIT(MCUCSR, MCUCSR_ISC2_BIT);
+					break;
+					case EXTI_RISING_EDGE:
+					SET_BIT(MCUCSR, MCUCSR_ISC2_BIT);
+					break;
+				}
+				break;
+			}
+}
+void MCAL_EXTI_Disable( uint8_t externalInterruptLineNumber){
+		switch (externalInterruptLineNumber) {
+			case EXTI_LINE0:
+			CLR_BIT(GICR, GICR_INT0_BIT);
+			break;
+			case EXTI_LINE1:
+			CLR_BIT(GICR, GICR_INT1_BIT);
+			break;
+			case EXTI_LINE2:
+			CLR_BIT(GICR, GICR_INT2_BIT);
+			break;
+		}
+}
+void MCAL_EXTI_Update (EXTI_InterruptConfig_t* EXTI_Config){
+	MCAL_EXTI_Enable(EXTI_Config);
+}
--- MCAL/External Interrupt/ATmega32_EXTERNALINTERRUPTS.h
+++ MCAL/External Interrupt/ATmega32_EXTERNALINTERRUPTS.h
@@ -1,16 +1,107 @@
 /*
- * IncFile1.h
+ * ATmega32_EXTERNALINTERRUPTS.h
  *
  * Created: 7/30/2023 3:55:48 AM
- *  Author: ahmed
+ *  Author: Ahmed Aref Omaira
  */ 
 
 
-#ifndef INCFILE1.HATMEGA32_EXTERNALINTERRUPTS_H_
-#define INCFILE1.HATMEGA32_EXTERNALINTERRUPTS_H_
+#ifndef ATMEGA32_EXTERNALINTERRUPTS_H_
+#define ATMEGA32_EXTERNALINTERRUPTS_H_
+/******************************************
+ *                                        *
+ *                INCLUDES                *
+ * 										  *
+ ******************************************/
+#include "ATmega32_REGISTERS.h"
+#include "COMMONMACROS.h"
+#include "PLATFORMTYPES.h"
 
 
+/**********************************************
+ *          	                              *
+ *   PIN CONFIGURATION TYPEDEF USED BY USER   *
+ *       	                                  *
+ **********************************************/
+typedef struct{
+	// Specifies the External Interrupt GPIO mapping
+	// This parameter must be set based on @ref EXTI_LineNumber define.
+	uint16_t EXTI_LineNumber; 
+
+	// Specifies Rising or Falling or both trigger.
+	// This parameter must be set based on @ref EXTI_Trigger_define.
+	uint8_t EXTI_TriggerCase;
+
+	// Set the C function() which will be called once IRQ happen.
+	void(*P_IRQ_CallBack)(void);
+
+}EXTI_InterruptConfig_t;
 
 
+/**************************************************
+ *                                                *
+ *   CONFIGURATION MACROS REFRENCES DEFINITIONS   *
+ *                                                *
+ **************************************************/
+/*******************************************
+ *        @ref EXTI_LineNumber define      *
+ * *****************************************/
+#define EXTI_LINE0		0
+#define EXTI_LINE1		1
+#define EXTI_LINE2		2
 
-#endif /* INCFILE1.HATMEGA32_EXTERNALINTERRUPTS_H_ */
+/*******************************************
+ *           @ref EXTI_Trigger_define        *
+ * *****************************************/
+#define EXTI_FALLING_EDGE	0
+#define EXTI_RISING_EDGE	1
+#define EXTI_LOW_LEVEL		2
+#define EXTI_ON_CHANGE		3
+
+/*****************************************************
+ *                                                   *
+ * EXTERNAL INTERRUPT INTERFACING MACROS DEFINITIONS *
+ *                                                   *
+ *****************************************************/
+/********* EXTERNAL INTERRUPT 0 BITS **************/
+#define GICR_INT0_BIT	6
+#define MCUCR_ISC00_BIT	0
+#define MCUCR_ISC01_BIT	1
+#define GIFR_INTF0_BIT	6
+
+/********* EXTERNAL INTERRUPT 1 BITS **************/
+#define GICR_INT1_BIT	7
+#define MCUCR_ISC10_BIT	2
+#define MCUCR_ISC11_BIT	3
+#define GIFR_INTF1_BIT	7
+
+/********* EXTERNAL INTERRUPT 2 BITS **************/
+#define GICR_INT2_BIT	5
+#define MCUCSR_ISC2_BIT	2
+#define GIFR_INTF2_BIT	5
+
+
+/******************************************
+ *                                        *
+ *  APIs Supported by "MCAL EXTI DRIVER"  *
+ *  									  *
+ ******************************************/
+void MCAL_EXTI_Enable( EXTI_InterruptConfig_t* interruptConfiguration );
+void MCAL_EXTI_Disable( uint8_t externalInterruptLineNumber);
+void MCAL_EXTI_Update (EXTI_InterruptConfig_t* EXTI_Config);
+
+
+/******************************************
+ *                                        *
+ *		IRQ HANDLERS DECLRATION			  *
+ *  									  *
+ ******************************************/
+void (*GPtr_IRQCallBack[3])(void) = {NULL};
+
+#define 			EXTI0_IRQHandler 					__vector_1
+#define 			EXTI1_IRQHandler 					__vector_2
+#define 			EXTI2_IRQHandler 					__vector_3
+#define 			ISR(INT_VECT)						void INT_VECT(void) __attribute__ ((signal,used));\
+														void INT_VECT(void)
+
+#endif /* ATMEGA32_EXTERNALINTERRUPTS_H_ */
