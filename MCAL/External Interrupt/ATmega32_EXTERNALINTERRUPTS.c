2596486c416188fa653ad26ee79a42caf907789f
Ahmed Aref <109268990+ahmedaref-1@users.noreply.github.com>
2023-08-01 05:12:43 +0300

    Update ATmega32_EXTERNALINTERRUPTS.c

--- MCAL/External Interrupt/ATmega32_EXTERNALINTERRUPTS.c
+++ MCAL/External Interrupt/ATmega32_EXTERNALINTERRUPTS.c
@@ -21,8 +21,8 @@ ISR(EXTI0_IRQHandler) {
 	if (GPtr_IRQCallBack[0] != NULL) {
 		//Call Back C function() which will be called once IRQ happen
 		GPtr_IRQCallBack[0]();
-		//This bit is cleared by writing a ‘1’ into the bit
-		SET_BIT(GIFR, GIFR_INTF0_BIT);
+		//This bit is cleared by writing a â€˜1â€™ into the bit
+		GIFR->INTF0 = HIGH;
 	}
 }
 
@@ -30,8 +30,8 @@ ISR(EXTI1_IRQHandler) {
 	if (GPtr_IRQCallBack[1] != NULL) {
 		//Call Back C function() which will be called once IRQ happen
 		GPtr_IRQCallBack[1]();
-		//This bit is cleared by writing a ‘1’ into the bit
-		SET_BIT(GIFR, GIFR_INTF1_BIT);
+		//This bit is cleared by writing a â€˜1â€™ into the bit
+		GIFR->INTF1 = HIGH;
 	}
 }
 
@@ -39,8 +39,8 @@ ISR(EXTI2_IRQHandler) {
 	if (GPtr_IRQCallBack[2] != NULL) {
 		//Call Back C function() which will be called once IRQ happen
 		GPtr_IRQCallBack[2]();
-		//This bit is cleared by writing a ‘1’ into the bit
-		SET_BIT(GICR, GICR_INT2_BIT);
+		//This bit is cleared by writing a â€˜1â€™ into the bit
+		GIFR->INTF2 = HIGH;
 	}
 }
 
@@ -58,63 +58,63 @@ void MCAL_EXTI_Enable( EXTI_InterruptConfig_t* interruptConfiguration){
 			// 2.Choose which external interrupt signal 
 			switch (interruptConfiguration->EXTI_LineNumber) {
 				case EXTI_LINE0:
-				// 3.Enable the choosen external interrupt in the General Interupt Control Register
-				SET_BIT(GICR, GICR_INT0_BIT);
-				// 4.Pass the Call Back Pointer to Function to the global array to be exectued by the ISR
+				// 3.Enable the chosen external interrupt in the General Interrupt Control Register
+				GICR->INT0 = HIGH;
+				// 4.Pass the Call Back Pointer to Function to the global array to be executed by the ISR
 				GPtr_IRQCallBack[0] = interruptConfiguration->P_IRQ_CallBack;
 				// 5.Choose the Interrupt Sense
 				switch (interruptConfiguration->EXTI_TriggerCase) {
 					case EXTI_LOW_LEVEL:
-					CLR_BIT(MCUCR, MCUCR_ISC00_BIT);
-					CLR_BIT(MCUCR, MCUCR_ISC01_BIT);
+					MCUCR->ISC00 = LOW;
+					MCUCR->ISC01 = LOW;
 					break;
 					case EXTI_ON_CHANGE:
-					SET_BIT(MCUCR, MCUCR_ISC00_BIT);
-					CLR_BIT(MCUCR, MCUCR_ISC01_BIT);
+					MCUCR->ISC00 = HIGH;
+					MCUCR->ISC01 = LOW;
 					break;
 					case EXTI_FALLING_EDGE:
-					CLR_BIT(MCUCR, MCUCR_ISC00_BIT);
-					SET_BIT(MCUCR, MCUCR_ISC01_BIT);
+					MCUCR->ISC00 = LOW;
+					MCUCR->ISC01 = HIGH;
 					break;
 					case EXTI_RISING_EDGE:
-					CLR_BIT(MCUCR, MCUCR_ISC00_BIT);
-					SET_BIT(MCUCR, MCUCR_ISC01_BIT);
+					MCUCR->ISC00 = LOW;
+					MCUCR->ISC01 = HIGH;
 					break;
 				}
 				break;
 
 				case EXTI_LINE1:
 				GPtr_IRQCallBack[1] = interruptConfiguration->P_IRQ_CallBack;
-				SET_BIT(GICR, GICR_INT1_BIT);
+				GICR->INT1 = HIGH;
 				switch (interruptConfiguration->EXTI_TriggerCase) {
 					case EXTI_LOW_LEVEL:
-					CLR_BIT(MCUCR, MCUCR_ISC10_BIT);
-					CLR_BIT(MCUCR, MCUCR_ISC11_BIT);
+					MCUCR->ISC10 = LOW;
+					MCUCR->ISC11 = LOW;;
 					break;
 					case EXTI_ON_CHANGE:
-					SET_BIT(MCUCR, MCUCR_ISC10_BIT);
-					CLR_BIT(MCUCR, MCUCR_ISC11_BIT);
+					MCUCR->ISC10 = HIGH;
+					MCUCR->ISC11 = LOW;
 					break;
 					case EXTI_FALLING_EDGE:
-					CLR_BIT(MCUCR, MCUCR_ISC10_BIT);
-					SET_BIT(MCUCR, MCUCR_ISC11_BIT);
+					MCUCR->ISC10 = LOW;
+					MCUCR->ISC11 = HIGH;
 					break;
 					case EXTI_RISING_EDGE:
-					CLR_BIT(MCUCR, MCUCR_ISC10_BIT);
-					SET_BIT(MCUCR, MCUCR_ISC11_BIT);
+					MCUCR->ISC10 = LOW;
+					MCUCR->ISC11 = HIGH;
 					break;
 				}
 				break;
 
 				case EXTI_LINE2:
 				GPtr_IRQCallBack[2] = interruptConfiguration->P_IRQ_CallBack;
-				SET_BIT(GICR, GICR_INT2_BIT);
+				GICR->INT2 = HIGH;
 				switch (interruptConfiguration->EXTI_TriggerCase) {
 					case EXTI_FALLING_EDGE:
-					CLR_BIT(MCUCSR, MCUCSR_ISC2_BIT);
+					MCUCSR->ISC2 = LOW;
 					break;
 					case EXTI_RISING_EDGE:
-					SET_BIT(MCUCSR, MCUCSR_ISC2_BIT);
+					MCUCSR->ISC2 = HIGH;
 					break;
 				}
 				break;
@@ -123,13 +123,13 @@ void MCAL_EXTI_Enable( EXTI_InterruptConfig_t* interruptConfiguration){
 void MCAL_EXTI_Disable( uint8_t externalInterruptLineNumber){
 		switch (externalInterruptLineNumber) {
 			case EXTI_LINE0:
-			CLR_BIT(GICR, GICR_INT0_BIT);
+			GICR->INT0 = LOW;
 			break;
 			case EXTI_LINE1:
-			CLR_BIT(GICR, GICR_INT1_BIT);
+			GICR->INT1 = LOW;
 			break;
 			case EXTI_LINE2:
-			CLR_BIT(GICR, GICR_INT2_BIT);
+			GICR->INT2 = LOW;
 			break;
 		}
 }
